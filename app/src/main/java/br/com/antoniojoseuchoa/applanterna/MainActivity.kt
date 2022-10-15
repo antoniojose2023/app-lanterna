19464977fe2ea73b7872333e91dd1e24f050706a
devmobile <antoniojoseuchoa2020@gmail.com>
2022-10-15 15:26:34 -0300

    Commit implementando o método ascender lanterna dispositivo e logo do app

--- app/src/main/java/br/com/antoniojoseuchoa/applanterna/MainActivity.kt
+++ app/src/main/java/br/com/antoniojoseuchoa/applanterna/MainActivity.kt
@@ -2,9 +2,11 @@ package br.com.antoniojoseuchoa.applanterna
 
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.hardware.camera2.CameraManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import br.com.antoniojoseuchoa.applanterna.databinding.ActivityMainBinding
@@ -18,6 +20,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        supportActionBar!!.title = "Lanterna"
+        supportActionBar!!.elevation = 0f
+
 
         binding.cardAtivar.setOnClickListener {
             if(!binding.swAtivaDesativa.isChecked){
@@ -39,18 +45,29 @@ class MainActivity : AppCompatActivity() {
            binding.cardAtivar.setCardBackgroundColor(Color.GRAY)
            binding.ivLanterna.setImageResource(R.drawable.ic_lanterna_ligada)
            state = true
+           ascenderLaternaDispositivo(state)
         }else{
             binding.tvAtivadoDesativado.text = "Desativado"
             binding.cardAtivar.setCardBackgroundColor(Color.WHITE)
             binding.ivLanterna.setImageResource(R.drawable.ic_lanterna_apagada)
             state = false
+            ascenderLaternaDispositivo(state)
         }
 
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun ascenderLaternaDispositivo(state: Boolean){
+           val cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
 
+           try{
+               var idCamera: String = cameraManager.cameraIdList[0]
+               cameraManager.setTorchMode(idCamera, state)
+
+           }catch (e: java.lang.Exception){
+               Log.i("erroCamera", "ascenderLaternaDispositivo: ${e.message}")
+           }
     }
 
 }
